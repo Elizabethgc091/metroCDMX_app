eb551e1cc9083ee113ed1450f78c86e10ff17037
Elizabeth Gonzalez <elizabeth.gc091@gmail.com>
2022-08-03 18:17:08 -0500

    desig page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-
+import logoMetro from './source/logoMetro.webp'
 import {Form} from "./components/form/Form";
 
 function App() {
   return (
     <div className="App">
-          <div>
-
-          <Form/>
+              <div className="header-container">
+                  <div className="me-auto p-2 logo-metro">
+                      <img id='logo-metro' src={logoMetro} alt="logo del metro"/>
+                      <p className="fs-4">Sistema de transporte colectivo</p>
+                  </div>
+                  <div className="p-2">Home</div>
+                  <div className="p-2">Lineas del metro</div>
+              </div>
+              <div className='main'>
+                  <Form/>
+              </div>
+              <div className="footer">
+                  <div className="p-2 flex-grow-1">Elizabeth González</div>
+                  <div className="p-2">Flex item</div>
+                  <div className="p-2">Third flex item</div>
+              </div>
           </div>
-    </div>
   );
 }
 
--- src/components/ContainerForm.js
+++ src/components/ContainerForm.js
@@ -30,49 +30,51 @@ export function ContainerForm({addElements}) {
     function handlerSubmit(e) {
         e.preventDefault();
         let date = new Date();
-        setValues({...values, ["created_at"]:date});
+        setValues({...values,"created_at":date});
         addElements(values);
         e.target.reset();
         setIsChecked(false)
     }
 
-
-
-
     return (
         <div>
-
             <form className='container' onSubmit={handlerSubmit}>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Title</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                    <label htmlFor="exampleInputEmail1" className="form-label color-primary">Título</label>
+                    <input type="text" className="form-control border-orange" id="exampleInputEmail1" aria-describedby="emailHelp"
                            name="title" onChange={handlerChange}/>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">brief</label>
-                    <input type="text" className="form-control" id="exampleInputPassword1" name="brief"
+                    <label htmlFor="exampleInputPassword1" className="form-label color-primary">Resumen</label>
+                    <input type="text" className="form-control border-orange" id="exampleInputPassword1" name="brief"
                            onChange={handlerChange}/>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label">content</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" name="content"
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label color-primary">Contenido</label>
+                    <textarea className="form-control border-orange" id="exampleFormControlTextarea1" rows="3" name="content"
                               onChange={handlerChange}></textarea>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">images</label>
-                    <input type="text" className="form-control" id="exampleInputPassword1" name="images"
+                    <label htmlFor="exampleInputPassword1" className="form-label color-primary">Imagenes</label>
+                    <input type="text" className="form-control border-orange" id="exampleInputPassword1" name="images"
                            onChange={handlerChange} />
                 </div>
+                <div className="d-flex mb-3">
+                    <div className="me-auto p-2">
+                        <div className="mb-3 form-check">
+                            <input type="checkbox" className="form-check-input border-orange" id="exampleCheck1" name="isTopNew"
+                                   onChange={handlerChange}
+                                   checked={isChecked}
+                            />
+                            <label className="form-check-label color-primary" htmlFor="exampleCheck1" >Noticia Top</label>
 
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" name="isTopNew"
-                           onChange={handlerChange}
-                           checked={isChecked}
-                        />
-                    {isChecked ? isChecked : "NOT "}
-                    <label className="form-check-label" htmlFor="exampleCheck1" >isTopNew</label>
+                        </div>
+                    </div>
+                    <div className="p-2"> <button type="submit" className="btn btn-primary" >Enviar</button></div>
                 </div>
-                <button type="submit" className="btn btn-primary" >Submit</button>
+
+
+
             </form>
         </div>
     )
