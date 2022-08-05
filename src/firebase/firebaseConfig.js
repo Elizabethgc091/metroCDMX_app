688ead99457b76d966ab2e0ad0debf753c2f6356
Elizabeth Gonzalez <elizabeth.gc091@gmail.com>
2022-08-05 17:30:39 -0500

    config .env

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,25 +1,11 @@
-/*
 export const firebaseConfig = {
-    apiKey: process.env.APP_METRO_API_KEY,
-    authDomain: process.env.APP_METRO_AUTH_DOMAIN,
-    databaseURL: process.env.APP_METRO_DATA_BASE_URL,
-    projectId: process.env.APP_METRO_PROJECT_ID,
-    storageBucket: process.env.APP_METRO_STORAGE_BUCKET,
-    messagingSenderId: process.env.APP_METRO_MESSAGING_SENDER_ID,
-    appId: process.env.APP_METRO_APP_ID,
-    measurementId: process.env.APP_METRO_MEASUREMENT_ID
-};
-*/
-
-
-export const firebaseConfig = {
-    apiKey: "AIzaSyDAvHCRZuVzvKvVcbYPQ8_rKeEGdUYPaKs",
-    authDomain: "metro-cdmx-development.firebaseapp.com",
-    databaseURL: "https://metro-cdmx-development-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "metro-cdmx-development",
-    storageBucket: "metro-cdmx-development.appspot.com",
-    messagingSenderId: "37724636360",
-    appId: "1:37724636360:web:ab466eab9f433f8b1911fd",
-    measurementId: "G-6LBYD762R2"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATA_BASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MEASUREMENT_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
